947880f1f6c97d12607a5eddf8efae0a5cbf3969
Your Name <sasilva1998@gmail.com>
2020-10-01 13:56:01 -0500

    formatted black

--- test/main.py
+++ test/main.py
@@ -1,9 +1,10 @@
 from xl320 import *
 import time
-dxl=xl320(baudrate=1000000, serialid=0)
-dxl.set_control_mode(1,1)
+
+dxl = xl320(baudrate=1000000, serialid=0)
+dxl.set_control_mode(1, 1)
 while True:
-    dxl.goal_speed(1,500)
+    dxl.goal_speed(1, 500)
     time.sleep(3)
-    dxl.goal_speed(1,1500)
+    dxl.goal_speed(1, 1500)
     time.sleep(3)
